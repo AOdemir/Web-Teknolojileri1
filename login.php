11ab7d74b0767559bbe76206d5509d7672d014ae
AOdemir <44072191+AOdemir@users.noreply.github.com>
2022-05-16 20:15:13 +0300

    update

---
+++ login.php
@@ -0,0 +1,78 @@
+<?php 
+
+session_start(); 
+
+
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+
+    function validate($data){
+
+       $data = trim($data);
+
+       $data = stripslashes($data);
+
+       $data = htmlspecialchars($data);
+
+       return $data;
+
+    }
+
+    $uname = validate($_POST['uname']);
+
+    $pass = validate($_POST['password']);
+
+    if (empty($uname)) {
+
+        header("Location: index.php?error=User Name is required");
+
+        exit();
+
+    }else if(empty($pass)){
+
+        header("Location: index.php?error=Password is required");
+
+        exit();
+
+    }else{
+
+        
+
+        if (1==1) {
+
+           
+
+            if ('b171210103@ogr.sakarya.edu.tr' === $uname &&  'b171210103' === $pass) {
+
+                echo "Logged in!";
+
+               
+
+                header("Location: giris.html");
+
+                exit();
+
+            }else{
+
+                header("Location: index.php?error=Incorect User name or password");
+
+                exit();
+
+            }
+
+        }else{
+
+            header("Location: index.php?error=Incorect User name or password");
+
+            exit();
+
+        }
+
+    }
+
+}else{
+
+    header("Location: index.php");
+
+    exit();
+
+}
